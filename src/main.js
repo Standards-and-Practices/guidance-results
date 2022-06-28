d178713ff16ad80fcbab2ec8b10736211e3be71a
David Petersen <dp@davidp.us>
2022-06-28 09:09:39 -0600

    Bump js.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { defineCustomElement } from 'vue';
-import DomainButtonsComponent from './components/DomainButtons.c3.vue'
+// import { defineCustomElement } from 'vue';
+// import DomainButtonsComponent from './components/DomainButtons.c3.vue'
 
-const DomainButtons = defineCustomElement(DomainButtonsComponent);
-customElements.define('wgu-domain-buttons', DomainButtons);
+// const DomainButtons = defineCustomElement(DomainButtonsComponent);
+// customElements.define('wgu-domain-buttons', DomainButtons);
+
+import { createApp } from 'vue'
+import DomainButtons from './components/DomainButtons.vue'
+
+createApp(DomainButtons).mount('#app')
